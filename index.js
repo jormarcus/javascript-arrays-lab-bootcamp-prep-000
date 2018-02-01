b7703666f1314bb599004ca61a22b410e6777afd
Jordan Marcus <jormarcus92@gmail.com>
2018-02-01 16:48:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+
+}
+
+function destructivelyPrependKitten(name) {
+
+}
+
+function destructivelyRemoveLastKitten() {
+
+}
+
+function destructivelyRemoveFirstKitten() {
+
+}
+
+
+function appendKitten(name) {
+
+}
+
+function prependKitten(name) {
+
+}
+
+function removeLastKitten() {
+
+}
+
+function removeFirstKitten() {
+  
+}
