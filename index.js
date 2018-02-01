cde1b9bd3db99fcb76b75473c11f00733d520f96
Jordan Marcus <jormarcus92@gmail.com>
2018-02-01 16:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-
+  kittens.push(name);
+  return kittens;
 }
 
 function destructivelyPrependKitten(name) {
